09440869aad621a121e44abce2a27b25120f061c
Adam Bien <abien@adam-bien.com>
2015-12-09 09:14:53 +0100

    Monitorable bulk heads

--- misc/src/main/java/com/airhacks/XMasResource.java
+++ misc/src/main/java/com/airhacks/XMasResource.java
@@ -1,16 +1,17 @@
 package com.airhacks;
 
+import com.airhacks.porcupine.execution.boundary.Dedicated;
 import static java.util.concurrent.CompletableFuture.supplyAsync;
+import java.util.concurrent.ExecutorService;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
-import javax.annotation.Resource;
 import javax.ejb.Stateless;
-import javax.enterprise.concurrent.ManagedExecutorService;
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.container.AsyncResponse;
 import javax.ws.rs.container.Suspended;
+import static java.util.concurrent.CompletableFuture.supplyAsync;
 
 /**
  *
@@ -20,8 +21,9 @@ import javax.ws.rs.container.Suspended;
 @Path("xmas")
 public class XMasResource {
 
-    @Resource
-    ManagedExecutorService mes;
+    @Inject
+    @Dedicated
+    ExecutorService xmasPipeline;
 
     @Inject
     GiftFactory factory;
@@ -30,7 +32,7 @@ public class XMasResource {
     public void msg(@Suspended AsyncResponse response) {
         Consumer<String> giftAcceptor = response::resume;
         Supplier<String> giftFactory = factory::nextGift;
-        supplyAsync(giftFactory, mes).thenAccept(giftAcceptor);
+        supplyAsync(giftFactory, xmasPipeline).thenAccept(giftAcceptor);
     }
 
 }
